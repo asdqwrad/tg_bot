b2cda48798bede79e27260a428bce287b2fccdd3
asdqwrad <103034325+asdqwrad@users.noreply.github.com>
2022-12-21 01:30:37 +0600

    Update main.py

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -4,9 +4,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 import requests
-from requests_html import AsyncHTMLSession
-from bs4 import BeautifulSoup
-from lxml import etree
 import json
 import pandas as pd
 
@@ -22,12 +19,94 @@ class Form(StatesGroup):
     query = State()
 
 
+profs = {
+    0: 'Новичок',
+    1: 'Мечник',
+    2: 'Маг',
+    3: 'Лучник',
+    4: 'Послушник',
+    5: 'Торговец',
+    6: 'Вор',
+    7: 'Рыцарь',
+    8: 'Священник',
+    9: 'Волшебник',
+    10: 'Кузнец',
+    11: 'Охотник',
+    12: 'Убийца',
+    14: 'Крестоносец',
+    15: 'Монах',
+    16: 'Мудрец',
+    17: 'Разбойник',
+    18: 'Алхимик',
+    19: 'Бард',
+    20: 'Танцовщица',
+    23: 'Супер Новичок',
+    24: 'Стрелок',
+    25: 'Ниндзя',
+    4008: 'Командор',
+    4009: 'Епископ',
+    4010: 'Архимаг',
+    4011: 'Оружейник',
+    4012: 'Снайпер',
+    4013: 'Ассасин',
+    4015: 'Паладин',
+    4016: 'Мистик',
+    4017: 'Профессор',
+    4018: 'Сталкер',
+    4019: 'Биохимик',
+    4020: 'Менестрель',
+    4021: 'Цыганка',
+    4046: 'Таэквондист',
+    4047: 'Гладиатор',
+    4049: 'Медиум',
+    4060: 'Рунмейстер',
+    4061: 'Чародей',
+    4062: 'Рейнджер',
+    4063: 'Архиепископ',
+    4064: 'Механик',
+    4065: 'Каратель',
+    4073: 'Тамплиер',
+    4074: 'Элементалист',
+    4075: 'Маэстро',
+    4076: 'Муза',
+    4077: 'Отшельник',
+    4078: 'Генетик',
+    4079: 'Преследователь',
+    4190: 'Экспертный Новичок',
+    4211: 'Кагеро',
+    4212: 'Оборо',
+    4215: 'Мятежник',
+    4218: 'Призыватель',
+    4239: 'Звёздный лорд',
+    4240: 'Жнец',
+    4252: 'Драгонмейстер',
+    4253: 'Мейстер',
+    4254: 'Сумрачный Каратель',
+    4255: 'Чернокнижник',
+    4256: 'Кардинал',
+    4257: 'Разведчик',
+    4258: 'Имперский страж',
+    4259: 'Биолог',
+    4260: 'Преследователь бездны',
+    4261: 'Мастер стихий',
+    4262: 'Инквизитор',
+    4263: 'Трубадур',
+    4264: 'Трувера',
+    4302: 'Небесный император',
+    4303: 'Духовный аскет',
+    4304: 'Шинкиро',
+    4305: 'Ширануи',
+    4306: 'Дозорный',
+    4307: 'Гипер Новичок',
+    4308: 'Повелитель духов'
+}
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     await message.answer(
         'You can control me by sending these commands:\n\n/item - случайный предмет\n/store - случайная '
-        'лавка\n/player - случайный игрок\n/roll - случайное значение от 1 до 6\n/youtube - видео с '
-        'наибольшим количеством просмотров')
+        'лавка\n/player - случайный игрок\n/roll - случайное значение от 1 до 6\n/youtube - поиск видео')
 
 
 @dp.message_handler(commands=['item'])
@@ -86,7 +165,7 @@ async def send_welcome(message: types.Message):
     base = info['base_level']
     prof = info['profession']
     sep = '-' * 30
-    text = f'{sep}\n{name}\n{sep}\nБазовый уровень: {base}\n{sep}\nПрофессиональный уровень: {prof}\n{sep}'
+    text = f'{sep}\n{name}\n{sep}\nБазовый уровень: {base}\n{sep}\nПрофессия: {profs[prof]}\n{sep}'
     await message.answer(f'`{text}`', parse_mode='markdown')
 
 
@@ -104,17 +183,7 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(state=Form.query)
 async def process_message(message: types.Message, state: FSMContext):
-    session = AsyncHTMLSession()
-    r = await session.get('https://www.youtube.com/results?search_query=' + message.text + '&sp=CAMSAhAB')
-    await r.html.arender(sleep=1)
-    soup = BeautifulSoup(r.html.html, 'html.parser')
-    dom = etree.HTML(str(soup))
-    elem = dom.xpath('//div[@id="contents"]//a')
-    if len(elem) == 0:
-        url = '`No results found`'
-    else:
-        url = 'https://www.youtube.com' + elem[0].xpath('@href')[0]
-    await message.answer(url, parse_mode='markdown')
+    await message.answer('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     await state.finish()
 
 
